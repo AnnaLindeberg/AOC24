9dc7bf1466185263c06922368b07101b9ac52650
AnnaLindeberg <anna@familjenlindeberg.com>
2024-12-01 19:32:43 +0100

    generate template script

---
+++ template.py
@@ -0,0 +1,13 @@
+# Day XX of Advent of Code 2024: YY
+# https://adventofcode.com/2024/day/XX
+
+def main():
+    with open("small_inputXX.txt") as file:
+        for row in file:
+            pass
+
+    print(f"Task 1: {0}\nTask 2: {0}")
+
+
+if __name__ == '__main__':
+    main()
